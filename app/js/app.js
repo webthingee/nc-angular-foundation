ae894e411d82187773d0a56aecb7987c763c8bba
Sean Lange <sean@webthingee.com>
2013-08-06 22:14:44 -0700

    add foundation stuff

--- app/js/app.js
+++ app/js/app.js
@@ -7,4 +7,9 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
     $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+  }])
+  .run(function($rootScope) {
+    $rootScope.$on('$viewContentLoaded', function () {
+      $(document).foundation();
+    });
+  });
